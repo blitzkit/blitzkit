62fed633108240dd64072d2c1cbe2b5ef8d49c88
TrèsAbhi <43380238+tresabhi@users.noreply.github.com>
2025-08-18 11:11:35 -0500

    fix: max width docs images

--- packages/website/src/components/Markdown.tsx
+++ packages/website/src/components/Markdown.tsx
@@ -1,8 +1,13 @@
 import { Link, Table } from '@radix-ui/themes';
 import MarkdownToJSX from 'markdown-to-jsx';
+import type { ComponentProps } from 'react';
 
 interface MarkdownProps {
   children: string;
+}
+
+function Image(props: ComponentProps<'img'>) {
+  return <img style={{ maxWidth: '100%' }} {...props} />;
 }
 
 export function Markdown({ children }: MarkdownProps) {
@@ -17,6 +22,7 @@ export function Markdown({ children }: MarkdownProps) {
           tr: { component: Table.Row },
           th: { component: Table.ColumnHeaderCell },
           td: { component: Table.Cell },
+          img: { component: Image },
         },
       }}
     >
