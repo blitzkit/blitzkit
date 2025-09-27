27c8b348bb20799ce83b3ddf68d07bd3a9f7878e
TrèsAbhi <43380238+tresabhi@users.noreply.github.com>
2025-09-27 13:39:08 -0500

    feat: favicons

--- packages/website/astro.config.mjs
+++ packages/website/astro.config.mjs
@@ -1,8 +1,30 @@
 // @ts-check
+import strings from "@blitzkit/i18n/strings/en.json";
+import favicons from "astro-favicons";
 import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
 
 export default defineConfig({
-  integrations: [react()],
+  devToolbar: { enabled: false },
+
+  integrations: [
+    favicons({
+      name: strings.common.name,
+      short_name: strings.common.name,
+
+      manifest: { start_url: "/", display_override: ["minimal-ui"] },
+
+      icons: {
+        windows: true,
+        android: true,
+        appleIcon: true,
+        appleStartup: true,
+        favicons: true,
+        yandex: true,
+      },
+    }),
+
+    react(),
+  ],
 });
