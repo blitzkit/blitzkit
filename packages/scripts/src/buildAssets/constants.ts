adc1791f56255a0287fda7d7a9004c8d36aaada7
TrèsAbhi <43380238+tresabhi@users.noreply.github.com>
2025-10-23 10:30:21 -0500

    feat: game archive mounting

--- packages/scripts/src/buildAssets/constants.ts
+++ packages/scripts/src/buildAssets/constants.ts
@@ -1,30 +1,25 @@
-import { readdir } from 'fs/promises';
-import { argv } from 'process';
+import { readdir } from "fs/promises";
+import { argv } from "process";
 
-const WINDOWS_APPS = 'C:/Program Files/WindowsApps';
+const WINDOWS_APPS = "C:/Program Files/WindowsApps";
 const wotbPackageRegex = /7458BE2C\..+_\d+(\.\d+)+_x64__.+/;
 
-const isDepot = argv.includes('--depot');
-const isManual = argv.includes('--manual');
+const isManual = argv.includes("--manual");
 
 let data: string;
 let compressed: boolean;
 
 if (isManual) {
-  data = '../../temp/game/Data';
-  compressed = true;
-} else if (isDepot) {
-  const [installationVersion] = await readdir(`../../depots/444202`);
-  data = `../../depots/444202/${installationVersion}/Data`;
+  data = "../../temp/game/Data";
   compressed = true;
 } else {
   const directories = await readdir(WINDOWS_APPS).then((files) =>
-    files.filter((file) => wotbPackageRegex.test(file)),
+    files.filter((file) => wotbPackageRegex.test(file))
   );
 
   if (directories.length !== 1) {
     throw new Error(
-      `Expected 1 directory for WoTB Windows App, got ${directories.length}.`,
+      `Expected 1 directory for WoTB Windows App, got ${directories.length}.`
     );
   }
 
