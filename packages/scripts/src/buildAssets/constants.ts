1aec1b28853b3b005ea07d660934c0a91c12f046
TrèsAbhi <43380238+tresabhi@users.noreply.github.com>
2025-12-10 17:13:12 -0600

    feat: add zip abilities

--- packages/scripts/src/buildAssets/constants.ts
+++ packages/scripts/src/buildAssets/constants.ts
@@ -3,24 +3,28 @@ import { AbstractVFS } from "../core/vfs/abstract";
 import { SteamVFS } from "../core/vfs/steam";
 import { ZipVFS } from "../core/vfs/zip";
 
+const steamPattern = /steam:(\d+)\/(\d+)/;
+const zipPattern = /zip:(.+)/;
+
 const source = Bun.argv
   .find((arg) => arg.startsWith("--source="))
   ?.split("=")[1];
 
 let resolved: AbstractVFS;
 
-if (source === "steam") {
-  const APP = 444200;
-  const DEPOT = 444202;
+if (!source) {
+  throw new Error("No source specified");
+} else if (steamPattern.test(source)) {
+  const [, app, depot] = source.match(steamPattern)!.map(Number);
 
   resolved = await new SteamVFS(
     assertSecret(import.meta.env.STEAM_USERNAME),
     assertSecret(import.meta.env.STEAM_PASSWORD),
-    APP,
-    DEPOT
+    app,
+    depot
   ).init();
-} else if (source === "zip") {
-  const url = assertSecret(import.meta.env.ZIP_URL);
+} else if (zipPattern.test(source)) {
+  const [, url] = source.match(zipPattern)!;
   resolved = await new ZipVFS(url).init();
 } else {
   throw new Error("No valid source specified");
