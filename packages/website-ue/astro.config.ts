428483cf4c3f586d806c50833a6f1fbb2a3fc178
TrèsAbhi <43380238+tresabhi@users.noreply.github.com>
2025-12-19 16:14:36 -0600

    fix: disable minifySyntax

--- packages/website-ue/astro.config.ts
+++ packages/website-ue/astro.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
     server: { allowedHosts: [] },
     esbuild: {
       minifyIdentifiers: true,
-      minifySyntax: true,
+      minifySyntax: false,
       minifyWhitespace: true,
     },
   },
