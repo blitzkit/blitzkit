0cb974acf51cdcdf33374266ef8f7c9659b790ae
TrèsAbhi <43380238+tresabhi@users.noreply.github.com>
2026-01-07 20:36:55 -0600

    feat: scales

--- packages/website-ue/astro.config.ts
+++ packages/website-ue/astro.config.ts
@@ -1,13 +1,11 @@
 import node from "@astrojs/node";
 import react from "@astrojs/react";
-import { wrapper } from "@blitzkit/i18n";
 import locales from "@blitzkit/i18n/locales.json";
 import strings from "@blitzkit/i18n/strings/en.json";
 import { mauve, mauveDark } from "@radix-ui/colors";
 import favicons from "astro-favicons";
 import { defineConfig } from "astro/config";
 import packageJSON from "../../package.json";
-import { tools } from "./src/core/tools/tools";
 
 const POSSIBLE_TARGETS = ["app", "web"];
 
@@ -48,35 +46,35 @@ export default defineConfig({
 
       version: packageJSON.version,
 
-      shortcuts: Object.values(tools)
-        .filter((tool) => !tool.disabled && !tool.href)
-        .map((tool) => {
-          const stringId = (tool.strings ??
-            tool.id) as keyof typeof strings.website.tools;
+      // shortcuts: Object.values(tools)
+      //   .filter((tool) => !tool.disabled && !tool.href)
+      //   .map((tool) => {
+      //     const stringId = (tool.strings ??
+      //       tool.id) as keyof typeof strings.website.tools;
 
-          return {
-            name: strings.website.tools[stringId].name,
-            name_localized: wrapper(
-              (strings) => strings.website.tools[stringId].name
-            ),
-            description: strings.website.tools[stringId].description,
-            description_localized: wrapper(
-              (strings) => strings.website.tools[stringId].description
-            ),
-            short_name: strings.website.tools[stringId].name,
-            short_name_localized: wrapper(
-              (strings) => strings.website.tools[stringId].name
-            ),
-            url: `/${tool.id}`,
-            icon: `public/assets/tools/${tool.id}.webp`,
-          };
-        }),
+      //     return {
+      //       name: strings.website.tools[stringId].name,
+      //       name_localized: wrapper(
+      //         (strings) => strings.website.tools[stringId].name
+      //       ),
+      //       description: strings.website.tools[stringId].description,
+      //       description_localized: wrapper(
+      //         (strings) => strings.website.tools[stringId].description
+      //       ),
+      //       short_name: strings.website.tools[stringId].name,
+      //       short_name_localized: wrapper(
+      //         (strings) => strings.website.tools[stringId].name
+      //       ),
+      //       url: `/${tool.id}`,
+      //       icon: `public/assets/tools/${tool.id}.webp`,
+      //     };
+      //   }),
 
       manifest: {
-        description: strings.website.home.seo_description,
-        description_localized: wrapper(
-          (strings) => strings.website.home.seo_description
-        ),
+        // description: strings.website.home.seo_description,
+        // description_localized: wrapper(
+        //   (strings) => strings.website.home.seo_description
+        // ),
 
         dir: "ltr",
         categories: ["utilities", "games", "developer", "developer tools"],
--- packages/website-ue/src/components/TextField/index.tsx
+++ packages/website-ue/src/components/TextField/index.tsx
@@ -2,5 +2,5 @@ import type { ComponentProps } from "react";
 import "./index.css";
 
 export function TextField(props: ComponentProps<"input">) {
-  return <input {...props} />;
+  return <input className="text-field" {...props} />;
 }
