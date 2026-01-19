323d624ec73510cfe44e500d21420cfdd85449c8
TrèsAbhi <43380238+tresabhi@users.noreply.github.com>
2026-01-18 18:38:26 -0600

    feat: idk bro

--- packages/website-ue/src/core/tools/tools.ts
+++ packages/website-ue/src/core/tools/tools.ts
@@ -1,46 +1,118 @@
-import type { ButtonProps } from "@radix-ui/themes";
+import {
+  ExclamationTriangleIcon,
+  GearIcon,
+  PersonIcon,
+} from "@radix-ui/react-icons";
+import type { IconProps } from "@radix-ui/react-icons/dist/types";
+import type { ComponentProps, ReactNode } from "react";
+import { TankIcon } from "../../icons/Tank";
+import type { Color } from "../ui/color";
 
 export interface Tool {
   id: string;
-  strings?: string;
 
-  button: ButtonProps["color"];
-  significant?: boolean;
-  disabled?: boolean;
-  href?: string;
-
+  accent: Color;
   banner_background_position: "top" | "center" | "bottom";
+
+  Icon:
+    | ((props: ComponentProps<"svg">) => ReactNode)
+    | ((props: IconProps) => ReactNode);
 }
 
 export const tools: Record<string, Tool> = {
   tanks: {
     id: "tanks",
-    strings: "tankopedia",
-    button: "purple",
-    significant: true,
 
+    Icon: TankIcon,
+    accent: "purple",
+    banner_background_position: "top",
+  },
+
+  players: {
+    id: "players",
+
+    Icon: PersonIcon,
+    accent: "purple",
+    banner_background_position: "top",
+  },
+
+  settings: {
+    id: "settings",
+
+    Icon: GearIcon,
+    accent: "gray",
     banner_background_position: "top",
   },
 
   avatars: {
     id: "avatars",
-    button: "gold",
 
+    Icon: ExclamationTriangleIcon,
+    accent: "gold",
     banner_background_position: "bottom",
   },
 
   backgrounds: {
     id: "backgrounds",
-    button: "gold",
 
+    Icon: ExclamationTriangleIcon,
+    accent: "gold",
     banner_background_position: "bottom",
   },
 
   api: {
     id: "api",
-    disabled: true,
-    button: "lime",
 
+    Icon: ExclamationTriangleIcon,
+    accent: "lime",
+    banner_background_position: "bottom",
+  },
+
+  compare: {
+    id: "compare",
+
+    Icon: ExclamationTriangleIcon,
+    accent: "crimson",
+    banner_background_position: "bottom",
+  },
+
+  performance: {
+    id: "performance",
+
+    Icon: ExclamationTriangleIcon,
+    accent: "jade",
+    banner_background_position: "bottom",
+  },
+
+  playlist: {
+    id: "playlist",
+
+    Icon: ExclamationTriangleIcon,
+    accent: "tomato",
+    banner_background_position: "bottom",
+  },
+
+  mixer: {
+    id: "mixer",
+
+    Icon: ExclamationTriangleIcon,
+    accent: "gray",
+    banner_background_position: "bottom",
+  },
+
+  guesser: {
+    id: "guesser",
+
+    Icon: ExclamationTriangleIcon,
+    accent: "cyan",
+    banner_background_position: "bottom",
+  },
+
+  session: {
+    id: "session",
+
+    Icon: ExclamationTriangleIcon,
+    accent: "blue",
     banner_background_position: "bottom",
   },
 };
