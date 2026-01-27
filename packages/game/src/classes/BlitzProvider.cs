dba56f894654736715da6a06c41794608e5307b1
TrèsAbhi <43380238+tresabhi@users.noreply.github.com>
2026-01-27 08:59:34 -0600

    feat: blitz provider

---
+++ packages/game/src/classes/BlitzProvider.cs
@@ -0,0 +1,28 @@
+﻿using CUE4Parse.FileProvider;
+using CUE4Parse.UE4.Versions;
+using Microsoft.JavaScript.NodeApi;
+
+namespace game.src.classes;
+
+[JSExport]
+public class BlitzProvider
+{
+    private readonly DefaultFileProvider provider;
+
+    public BlitzProvider(string directory)
+    {
+        provider = new(
+            directory: directory,
+            searchOption: SearchOption.AllDirectories,
+            versions: new(EGame.GAME_UE5_5),
+            pathComparer: StringComparer.OrdinalIgnoreCase
+        );
+        provider.Initialize();
+        provider.Mount();
+    }
+
+    public string[] Files()
+    {
+        return provider.Files.Keys.ToArray();
+    }
+}
--- packages/game/src/index.ts
+++ packages/game/src/index.ts
@@ -1,3 +1,1 @@
-console.log("that's crazy yo");
-
 export * from "../bin/Debug/net9.0/game";
