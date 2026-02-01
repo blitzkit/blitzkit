bf9262f1f1875a932ed770bb7f5b679e6ce9711d
TrèsAbhi <43380238+tresabhi@users.noreply.github.com>
2026-01-31 23:21:43 -0600

    feat: some work on footer

--- packages/website-ue/src/components/Link/index.tsx
+++ packages/website-ue/src/components/Link/index.tsx
@@ -1,21 +1,29 @@
 import type { ComponentProps } from "react";
 import { classNames } from "../../core/ui/classNames";
-import "./index.css";
+import type { Color } from "../../core/ui/color";
+import styles from "./index.module.css";
 
 export interface LinkProps extends ComponentProps<"a"> {
   underline?: "always" | "hover" | "never";
   highContrast?: boolean;
+  color?: Color | "accent";
 }
 
 export function Link({
   highContrast,
   className,
   underline = "always",
+  color = "accent",
+  style,
   ...props
 }: LinkProps) {
   return (
     <a
-      className={classNames("link", className)}
+      className={classNames(styles.link, className)}
+      style={{
+        color: `var(--${color}-${highContrast ? 12 : 11})`,
+        ...style,
+      }}
       {...props}
       data-underline={underline}
       data-high-contrast={highContrast}
