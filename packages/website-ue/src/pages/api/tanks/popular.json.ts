e50b8d11bfe794c0c83379c2184042bdaf0c9abe
TrèsAbhi <43380238+tresabhi@users.noreply.github.com>
2026-02-03 15:20:10 -0600

    feat: comment for future

--- packages/website-ue/src/pages/api/tanks/popular.json.ts
+++ packages/website-ue/src/pages/api/tanks/popular.json.ts
@@ -1,7 +1,9 @@
 import { api } from "../../../core/api/dynamic";
 
 export async function GET() {
-  throw new Error("MAKE THIS A PROTO AND ADD LAST UPDATED TIME");
+  throw new Error(
+    "MAKE THIS A PROTO AND ADD LAST UPDATED TIME OH AND ADD 0's FOR TANKS WITH NO ENTRIES",
+  );
   const popular = await api.popularTanks();
   return Response.json(popular);
 }
