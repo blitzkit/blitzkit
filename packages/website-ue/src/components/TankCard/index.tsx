a7bd671309bf01d6dbc353b3db2058e3c544778f
TrèsAbhi <43380238+tresabhi@users.noreply.github.com>
2026-03-15 22:42:13 -0500

    feat: tank card links

--- packages/website-ue/src/components/TankCard/index.tsx
+++ packages/website-ue/src/components/TankCard/index.tsx
@@ -1,16 +1,24 @@
 import { api } from "../../core/api/dynamic";
 import { useAwait } from "../../hooks/useAwait";
 import { useGameStrings } from "../../hooks/useGameStrings";
+import { useLocale } from "../../hooks/useLocale";
 import type { PopularTank } from "../../protos/popular_tanks";
+import { LinkI18n } from "../LinkI18n";
 import { Text } from "../Text";
 import styles from "./index.module.css";
 
 export function TankCard({ id }: PopularTank) {
   const tank = useAwait(() => api.tank(id), `tank-${id}`);
   const gameStrings = useGameStrings("TankEntity");
+  const locale = useLocale();
 
   return (
-    <div className={styles["tank-card"]}>
+    <LinkI18n
+      href={`/tanks/${tank.slug}`}
+      underline="never"
+      locale={locale}
+      className={styles["tank-card"]}
+    >
       <div className={styles["icon-wrapper"]}>
         <div
           className={styles.flag}
@@ -24,10 +32,12 @@ export function TankCard({ id }: PopularTank) {
           style={{
             backgroundImage: `url(/api/tanks/${id}.webp)`,
           }}
-        />
+        >
+          <div className={styles.cover} />
+        </div>
       </div>
 
       <Text className={styles.name}>{gameStrings[tank.tank!.name!.value]}</Text>
-    </div>
+    </LinkI18n>
   );
 }
