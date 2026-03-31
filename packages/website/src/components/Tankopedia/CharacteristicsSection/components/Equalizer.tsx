cffd37c6f134fed7938da304430bbeed18a2e400
TrèsAbhi <43380238+tresabhi@users.noreply.github.com>
2026-03-31 17:01:47 -0500

    feat: ui

---
+++ packages/website/src/components/Tankopedia/CharacteristicsSection/components/Equalizer.tsx
@@ -0,0 +1,43 @@
+import { asset } from "@blitzkit/core";
+import { Box, Flex, Heading, Switch, Text } from "@radix-ui/themes";
+import { useLocale } from "../../../../hooks/useLocale";
+import { ConfigurationChildWrapper } from "./ConfigurationChildWrapper";
+
+export function Equalizer() {
+  const { strings } = useLocale();
+
+  return (
+    <ConfigurationChildWrapper>
+      <Heading size="4">
+        {strings.website.tools.tankopedia.configuration.equalizer.title}
+      </Heading>
+
+      <Box
+        width="100%"
+        style={{
+          cursor: "pointer",
+          borderRadius: "var(--radius-3)",
+          backgroundImage: `url(${asset("icons/game_mode_banners/45.webp")})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          overflow: "hidden",
+        }}
+      >
+        <Flex
+          width="100%"
+          justify="between"
+          p="4"
+          style={{
+            background:
+              "linear-gradient(90deg, var(--black-a3), var(--gray-1))",
+          }}
+        >
+          <Text>
+            {strings.website.tools.tankopedia.configuration.equalizer.equalize}
+          </Text>
+          <Switch />
+        </Flex>
+      </Box>
+    </ConfigurationChildWrapper>
+  );
+}
