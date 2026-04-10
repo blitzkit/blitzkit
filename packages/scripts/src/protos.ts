9a1c09490f6125400b0220ff646c4daf3300cabb
TrèsAbhi <43380238+tresabhi@users.noreply.github.com>
2026-04-09 21:59:58 -0500

    feat: import google

--- packages/scripts/src/protos.ts
+++ packages/scripts/src/protos.ts
@@ -6,10 +6,7 @@ const TS_PROTO_EXECUTABLE_LOCATIONS = [
   "./node_modules/.bin/protoc-gen-ts_proto",
   "../../node_modules/.bin/protoc-gen-ts_proto",
 ];
-const ROOTS = [
-  "../../packages/closed/protos",
-  "../../packages/website-ue/src/protos",
-];
+const ROOT = "../../packages/closed/protos";
 
 let tsProtoExecutableLocation: string | undefined = undefined;
 
@@ -33,36 +30,34 @@ if (!tsProtoExecutableLocation) {
 
 await mkdir("temp", { recursive: true });
 
-for (const root of ROOTS) {
-  let args = "";
+let args = "";
 
-  for (const include of ROOTS) {
-    args += `-I=${include}\n`;
+args += `-I=${ROOT}\n`;
+args += `--plugin=${tsProtoExecutableLocation}\n`;
+args += "--ts_proto_opt=esModuleInterop=true\n";
+args += "--ts_proto_opt=oneof=unions-value\n";
+args += "--ts_proto_opt=unrecognizedEnum=false\n";
+args += "--ts_proto_opt=snakeToCamel=false\n";
+args += `--ts_proto_out=${ROOT}\n`;
+
+const filesRaw = await readdir(`${ROOT}`, { recursive: true });
+const files: string[] = [];
+
+for (const file of filesRaw) {
+  if (file.endsWith(".proto")) {
+    files.push(file);
+    args += `${ROOT}/${file}\n`;
   }
 
-  args += `--plugin=${tsProtoExecutableLocation}\n`;
-  args += "--ts_proto_opt=esModuleInterop=true\n";
-  args += "--ts_proto_opt=oneof=unions-value\n";
-  args += "--ts_proto_opt=unrecognizedEnum=false\n";
-  args += "--ts_proto_opt=snakeToCamel=false\n";
-  args += `--ts_proto_out=${root}\n`;
-
-  const filesRaw = await readdir(`${root}`);
-  const files: string[] = [];
-
-  for (const file of filesRaw) {
-    if (file.endsWith(".proto")) {
-      files.push(file);
-      args += `${root}/${file}\n`;
-    }
-
-    if (file.endsWith(".ts")) await rm(`${root}/${file}`);
-  }
-
-  await writeFile("temp/protoc.txt", args);
-  execFile("protoc", ["@temp/protoc.txt"], (error, stdout, stderr) => {
-    if (error) throw new Error(error.message);
-    if (stderr) console.error(stderr);
-    if (stdout) console.log(stdout);
-  });
+  if (file.endsWith(".ts")) await rm(`${ROOT}/${file}`);
 }
+
+await writeFile("temp/protoc.txt", args);
+
+// throw "";
+
+execFile("protoc", ["@temp/protoc.txt"], (error, stdout, stderr) => {
+  if (error) throw new Error(error.message);
+  if (stderr) console.error(stderr);
+  if (stdout) console.log(stdout);
+});
