03e06b050e53c5723fceef2ff5bf236270aa1fa7
tresabhi <43380238+tresabhi@users.noreply.github.com>
2026-05-18 16:45:36 -0500

    separate game interface import better

--- packages/website-ue/src/game/game.ts
+++ packages/website-ue/src/game/game.ts
@@ -1,10 +1,11 @@
 import { assertSecret } from "@blitzkit/core";
-import { GameInterface } from "@blitzkit/game";
+import type { GameInterface } from "@blitzkit/game";
 import { clientUnmounted } from "./unmounted";
 
 let _game: GameInterface | null = null;
 
 if (!clientUnmounted) {
+  const { GameInterface } = await import("@blitzkit/game");
   _game = new GameInterface(assertSecret(import.meta.env.PUBLIC_WOTB_CLIENT));
 }
 
