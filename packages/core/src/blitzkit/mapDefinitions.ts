d41d45abaef161550cbe2b6ac35da15568570049
tresabhi <43380238+tresabhi@users.noreply.github.com>
2026-08-01 14:48:18 -0500

    update (2)

--- packages/core/src/blitzkit/mapDefinitions.ts
+++
@@ -1,7 +0,0 @@
-import { fetchPB } from '../protobuf';
-import { MapDefinitions } from '../protos';
-import { asset } from './asset';
-
-export function fetchMapDefinitions() {
-  return fetchPB(asset('definitions/maps.pb'), MapDefinitions);
-}
--- packages/core/src/blitzkit/tankIcon.ts
+++ packages/core/src/blitzkit/tankIcon.ts
@@ -1,7 +1,5 @@
-import { asset } from "./asset";
-
 export const BLITZKIT_TANK_ICON_SIZE = { width: 40 * 16, height: 30 * 16 };
 
 export function tankIcon(id: number, size: "big" | "small" = "big") {
-  return asset(`icons/tanks/${size}/${id}.webp`);
+  return `/api/tanks/${id}/icons/${size}.webp`;
 }
