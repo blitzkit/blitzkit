85883cd2c542504eb6cc92220536b2fe1a96fd23
tresabhi <43380238+tresabhi@users.noreply.github.com>
2026-08-04 20:23:50 -0500

    faded flags

--- packages/website/src/pages/[...chunk_api]/flags/fade/[id].webp.ts
+++ packages/website/src/pages/[...chunk_api]/flags/fade/[id].webp.ts
@@ -6,19 +6,18 @@ import { getStaticPaths as _getStaticPaths } from "../../_index";
 export const getStaticPaths = mixStaticPaths(_getStaticPaths, async () => {
   const paths: GetStaticPathsItem[] = [];
 
-  const scratchedFiles = await vfs
-    .dir("Data/Gfx/Lobby/flags")
+  const fadedFiles = await vfs
+    .dir(`Data/Gfx/Lobby/flags`)
     .then((files) =>
       files.filter(
         (flag) =>
-          flag.startsWith("flag_tutor-tank_") &&
-          !flag.endsWith("@2x.packed.webp"),
+          flag.startsWith("flag_filter_") && flag.endsWith("@2x.packed.webp"),
       ),
     );
 
-  for (const flag of scratchedFiles) {
+  for (const flag of fadedFiles) {
     const path = `Data/Gfx/Lobby/flags/${flag}`;
-    const id = flag.match(/flag_tutor-tank_(.+)\.packed\.webp/)![1];
+    const id = flag.match(/flag_filter_(.+)@2x\.packed\.webp/)![1];
 
     paths.push({
       params: { id },
