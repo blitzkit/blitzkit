ffd74de79f0dc43dcfb8396c38bffadec8e7797c
tresabhi <43380238+tresabhi@users.noreply.github.com>
2026-08-08 14:02:20 -0500

    quiet all

--- packages/scripts/src/discoverIds/index.ts
+++ packages/scripts/src/discoverIds/index.ts
@@ -245,8 +245,8 @@ for (let i = 0; i < chunks.length; i++) {
 const git = $.cwd(WORKING_DIR);
 
 await git`git add .`.quiet();
-await git`git commit --amend -m "ids update ${new Date().toISOString()}"`;
-await git`git push --force-with-lease`;
+await git`git commit --amend -m "ids update ${new Date().toISOString()}"`.quiet();
+await git`git push --force-with-lease`.quiet();
 
 console.log(
   `Committed ${totalFound.toLocaleString()} total ids across ${chunkCount} chunks`,
