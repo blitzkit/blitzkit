5b4ad95a14bf83f7f819049d18d30890495e03c3
tresabhi <43380238+tresabhi@users.noreply.github.com>
2026-09-06 21:06:02 -0500

    clean up a bit

--- packages/website/src/config/characteristics.ts
+++ packages/website/src/config/characteristics.ts
@@ -24,42 +24,21 @@ export const characteristics = {
     },
   },
 
-  assault_damage_coefficient: {
-    type: CharacteristicType.Number,
-
-    compute({ gun, shell, assault_distance }) {
-      if (
-        !gun.assault_ranges ||
-        !gun.assault_ranges.types.includes(shell.type)
-      ) {
-        return 1;
-      }
-
-      return (
-        gun.assault_ranges.ranges.find(
-          ({ distance }) => distance >= assault_distance,
-        )?.factor ?? 0
-      );
-    },
-  },
-
-  armor_damage_coefficient: {
-    type: CharacteristicType.Number,
-
-    compute() {
-      let coefficient = 1;
-
-      return coefficient;
-    },
-  },
-
   damage: {
     type: CharacteristicType.Number,
-    compute({ characteristic, shell, equalizer }) {
+    compute({ characteristic, gun, shell, equalizer, assault_distance }) {
       let coefficient = 1;
 
-      coefficient *= characteristic("armor_damage_coefficient") as number;
-      coefficient *= characteristic("assault_damage_coefficient") as number;
+      if (gun.assault_ranges && gun.assault_ranges.types.includes(shell.type)) {
+        const match = gun.assault_ranges.ranges.find(
+          ({ distance }) => distance >= assault_distance,
+        );
+
+        if (match !== undefined) {
+          coefficient *= match.factor;
+        }
+      }
+
       coefficient *= equalizer.damage;
 
       return coefficient * shell.armor_damage;
