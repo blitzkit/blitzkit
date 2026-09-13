491b03ad00aed22988def70f5e18bc537a11ecfe
tresabhi <43380238+tresabhi@users.noreply.github.com>
2026-09-13 12:52:25 -0500

    proto compiling

--- packages/scripts/src/protos.ts
+++ packages/scripts/src/protos.ts
@@ -2,7 +2,7 @@ import { execFile } from "child_process";
 import { mkdir, readdir, rm, writeFile } from "fs/promises";
 
 const TEMP = "../../temp";
-const TS_PROTO_EXECUTABLE = "./node_modules/.bin/protoc-gen-ts_proto";
+const TS_PROTO_EXECUTABLE = "../../node_modules/.bin/protoc-gen-ts_proto";
 const ROOTS = ["../protos/src/blitzkit"];
 
 for (const root of ROOTS) {
@@ -17,12 +17,15 @@ for (const root of ROOTS) {
   args += "--ts_proto_opt=snakeToCamel=false\n";
   args += `--ts_proto_out=${root}\n`;
 
-  for (const dir of await readdir(root)) {
-    args += `-I=${root}/${dir}\n`;
+  for (const dir of ROOTS) {
+    args += `-I=${dir}\n`;
   }
 
   const filesRaw = await readdir(`${root}`, { recursive: true });
   const files: string[] = [];
+
+  let index = "";
+  index += "// @ts-nocheck\n";
 
   for (const file of filesRaw) {
     if (file.endsWith(".proto")) {
@@ -30,7 +33,13 @@ for (const root of ROOTS) {
       args += `${root}/${file}\n`;
     }
 
-    if (file.endsWith(".ts")) await rm(`${root}/${file}`);
+    if (file.endsWith(".ts")) {
+      const name = file.slice(0, -3);
+
+      index += `export * from "./${name}";\n`;
+
+      await rm(`${root}/${file}`);
+    }
   }
 
   await writeFile(`${TEMP}/protoc.txt`, args);
@@ -40,4 +49,6 @@ for (const root of ROOTS) {
     if (stderr) console.error(stderr);
     if (stdout) console.log(stdout);
   });
+
+  await writeFile(`${root}/index.ts`, index);
 }
